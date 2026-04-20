65ff3901015a58c8b2fde2006c2823a308b419db
alanzhangyihong <alanzhangyihong@gmail.com>
2026-04-20 15:55:52 +0800

    Update [[slug]].js

--- functions/post/[[slug]].js
+++ functions/post/[[slug]].js
@@ -117,7 +117,7 @@ function buildPage(meta) {
   <link rel="preconnect" href="https://fonts.googleapis.com"/>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
   <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800;900&family=Noto+Sans+SC:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
-  <link rel="stylesheet" href="/style.css"/>
+  <link rel="stylesheet" href="/elvonis-style.css"/>
 </head>
 <body style="padding-top:68px;">
 
@@ -208,7 +208,7 @@ function buildPage(meta) {
   </p>
 </footer>
 
-<script src="/language.js"></script>
+<script src="/elvonis-core.js"></script>
 <script>
   (function() {
     var p = document.getElementById('lang-pills-desktop');
